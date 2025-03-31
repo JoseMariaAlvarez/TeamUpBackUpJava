f768805762eab61ccf8190a827fc9f5189112efb
José María Álvarez Palomo <jmalvarez@uma.es>
2025-03-31 09:49:37 +0200

    Changed string formation in logger messages.

--- src/main/java/TeamUpBackup.java
+++ src/main/java/TeamUpBackup.java
@@ -42,7 +42,8 @@ import org.slf4j.LoggerFactory;
 public class TeamUpBackup {
 
     private static final String API_URL = "https://api.teamup.com/";
-    private final static String configFilePath = "./configuration.properties";
+    private final static String CONFIG_FILE_PATH = "./configuration.properties";
+    private final static String BACKUP_FILE_PATH_PREFIX = "./datos/backup-";
 
     private static final Logger logger = LoggerFactory.getLogger(TeamUpBackup.class);
 
@@ -53,67 +54,65 @@ public class TeamUpBackup {
         String startDate = null;
         String endDate = null;
 
-        logger.info("Starting Teamup backup " + new Date());
+        logger.info("Starting Teamup backup {}", new Date());
         try {
-            properties.load(new FileInputStream(new File(configFilePath)));
+            properties.load(new FileInputStream(new File(CONFIG_FILE_PATH)));
 
             apiKey = (String) properties.getProperty("API_KEY", "");
             calendarId = (String) properties.getProperty("CALENDAR_ID", "");
             startDate = (String) properties.getProperty("START_DATE", "");
             endDate = (String) properties.getProperty("END_DATE", "");
             if (apiKey.equals("")) {
-                logger.error("API key not found in configuration file " + configFilePath);
+                logger.error("API key not found in configuration file {}", CONFIG_FILE_PATH);
                 System.exit(1);
             }
             if (calendarId.equals("")) {
-                logger.error("Calendar id. not found in configuration file " + configFilePath);
+                logger.error("Calendar id. not found in configuration file {}", CONFIG_FILE_PATH);
                 System.exit(1);
             }
             if (startDate.equals("")) {
-                logger.error("Start date not found in configuration file " + configFilePath);
+                logger.error("Start date not found in configuration file {}", CONFIG_FILE_PATH);
                 System.exit(1);
             }
             if (endDate.equals("")) {
-                logger.error("End date not found in configuration file " + configFilePath);
+                logger.error("End date not found in configuration file {}", CONFIG_FILE_PATH);
                 System.exit(1);
             }
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             try {
                 LocalDate.parse(startDate, formatter);
             } catch (DateTimeParseException e) {
-                logger.error("Invalid start date format in configuration file "
-                        + configFilePath + ". Expected yyyy-MM-dd: " + e.getMessage());
+                logger.error("Invalid start date format in configuration file {}. Expected yyyy-MM-dd: {}", 
+                             CONFIG_FILE_PATH, e.getMessage());
                 System.exit(1);
             }
             try {
                 LocalDate.parse(endDate, formatter);
             } catch (DateTimeParseException e) {
-                logger.error("Invalid start date format in configuration file "
-                        + configFilePath + ". Expected yyyy-MM-dd: " + e.getMessage());
+                logger.error("Invalid end date format in configuration file {}. Expected yyyy-MM-dd: {}", 
+                             CONFIG_FILE_PATH, e.getMessage());
                 System.exit(1);
             }
         } catch (FileNotFoundException e) {
-            logger.error("Configuration file "
-                    + configFilePath + " not found: " + e.getMessage());
+            logger.error("Configuration file {} not found: {}", CONFIG_FILE_PATH, e.getMessage());
             System.exit(1);
         } catch (IOException e) {
-            logger.error("Error reading configuration file "
-                    + configFilePath + ": " + e.getMessage());
+            logger.error("Error reading configuration file {}: {}", CONFIG_FILE_PATH, e.getMessage());
             System.exit(1);
         } catch (IllegalArgumentException e) {
-            logger.error("Input stream contains a malformed Unicode escape sequence in configuration file "
-                    + configFilePath + ": " + e.getMessage());
+            logger.error("Input stream contains a malformed Unicode escape sequence in configuration file {}: {}", 
+                         CONFIG_FILE_PATH, e.getMessage());
             System.exit(1);
         }
 
         // get current day, month and year as a string separated by a dash
         LocalDate date = LocalDate.now();
-        // String backupFilePathIcs = "./backup-"
-        //         + calendarId + "-" + date.getYear()
-        //         + "-" + date.getMonthValue()
-        //         + "-" + date.getDayOfMonth()
-        //         + ".ics";
-        String backupFilePathCsv = "./backup-"
+        // String backupFilePathIcs = backupFilePathPrefix
+        // + calendarId + "-" + date.getYear()
+        // + "-" + date.getMonthValue()
+        // + "-" + date.getDayOfMonth()
+        // + ".ics";
+        String backupFilePathCsv = BACKUP_FILE_PATH_PREFIX
                 + calendarId + "-" + date.getYear()
                 + "-" + date.getMonthValue()
                 + "-" + date.getDayOfMonth()
@@ -124,41 +123,41 @@ public class TeamUpBackup {
             String subCalendars = fetchSubCalendars(apiKey, calendarId);
             subCalendarsMap = getSubcalendarIds(subCalendars);
         } catch (IOException e) {
-            logger.error("Error getting subcalendars: " + e.getMessage());
+            logger.error("Error getting subcalendars: {}", e.getMessage());
         }
         String eventsJson = null;
-        try{
-        eventsJson = fetchCalendarEvents(apiKey, calendarId, startDate,
-        endDate);
-        } catch (IOException e){
-        logger.error("Error fetching calendar events: " + e.getMessage());
-        System.exit(1);
+        try {
+            eventsJson = fetchCalendarEvents(apiKey, calendarId, startDate,
+                    endDate);
+        } catch (IOException e) {
+            logger.error("Error fetching calendar events: {}", e.getMessage());
+            System.exit(1);
         }
         // try {
-        //     String eventsExampleFile = "./events-example.json";
-        //     StringBuilder sb = new StringBuilder();
-        //     Files.readAllLines(Paths.get(eventsExampleFile),
-        //             StandardCharsets.UTF_8).stream().forEach(l -> sb.append(l));
-        //     eventsJson = sb.toString();
+        // String eventsExampleFile = "./events-example.json";
+        // StringBuilder sb = new StringBuilder();
+        // Files.readAllLines(Paths.get(eventsExampleFile),
+        // StandardCharsets.UTF_8).stream().forEach(l -> sb.append(l));
+        // eventsJson = sb.toString();
         // } catch (IOException e) {
-        //     logger.error("Error reading events example file: " + e.getMessage());
-        //     System.exit(1);
+        // logger.error("Error reading events example file: {}", e.getMessage());
+        // System.exit(1);
         // }
         // try {
-        //     Files.writeString(Path.of(backupFilePathIcs),
-        //             saveEventsToIcs(eventsJson),
-        //             StandardOpenOption.CREATE);
-        //     logger.info("Events saved to " + backupFilePathIcs);
+        // Files.writeString(Path.of(backupFilePathIcs),
+        // saveEventsToIcs(eventsJson),
+        // StandardOpenOption.CREATE);
+        // logger.info("Events saved to {}", backupFilePathIcs);
         // } catch (IOException e) {
-        //     logger.error("Error saving events to iCalendar file: " + e.getMessage());
+        // logger.error("Error saving events to iCalendar file: {}", e.getMessage());
         // }
         try {
             Files.writeString(Path.of(backupFilePathCsv),
                     saveEventsToCsv(eventsJson, subCalendarsMap),
                     StandardOpenOption.CREATE);
-            logger.info("Events saved to " + backupFilePathCsv);
+            logger.info("Events saved to {}", backupFilePathCsv);
         } catch (IOException e) {
-            logger.error("Error saving events to csv file: " + e.getMessage());
+            logger.error("Error saving events to csv file: {}", e.getMessage());
         }
     }
 
@@ -215,9 +214,9 @@ public class TeamUpBackup {
         }
     }
 
-    private static String saveEventsToCsv(String eventsJson, Map<Long, String> subCalendarsMap) throws IOException {
+    private static String saveEventsToCsv(String eventsJson, Map<Long, String> subCalendarsMap) {
         String csvEventsString = null;
-        try(StringWriter sw = new StringWriter(); PrintWriter writer = new PrintWriter(sw)){
+        try (StringWriter sw = new StringWriter(); PrintWriter writer = new PrintWriter(sw)) {
             Gson gson = new Gson();
             JsonObject jsonObject = gson.fromJson(eventsJson, JsonObject.class);
             JsonArray events = jsonObject.getAsJsonArray("events");
@@ -228,7 +227,7 @@ public class TeamUpBackup {
             DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
             for (JsonElement element : events) {
-                try{
+                try {
                     JsonObject event = element.getAsJsonObject();
                     Long calendarid = event.get("subcalendar_id").getAsLong();
                     String calendarName = "\"" + subCalendarsMap.get(calendarid) + "\"";
@@ -241,72 +240,73 @@ public class TeamUpBackup {
                     String description = org.jsoup.Jsoup.parse(htmlDescription).text();
 
                     if (calendarName != null && subject != null && start != null && end != null && who != null
-                            && location != null && description != null){
+                            && location != null && description != null) {
                         writer.printf("%s,%s,%s,%s,%s,%s,%s,%s,%s%n",
-                            calendarName,
-                            subject,
-                            start.format(dateFormatter),
-                            start.format(timeFormatter),
-                            end.format(dateFormatter),
-                            end.format(timeFormatter),
-                            who,
-                            location,
-                            description);
+                                calendarName,
+                                subject,
+                                start.format(dateFormatter),
+                                start.format(timeFormatter),
+                                end.format(dateFormatter),
+                                end.format(timeFormatter),
+                                who,
+                                location,
+                                description);
                     } else {
-                       logger.error("Missing data in event: " + element.toString());
+                        logger.error("Missing data in event: {}", element.toString());
                     }
-                } catch (Exception e){
-                    logger.error("Error processing event: " + element.toString() + ": " + e.getMessage());
+                } catch (Exception e) {
+                    logger.error("Error processing event {}: {}", element.toString(), e.getMessage());
                 }
             }
             csvEventsString = sw.toString();
-        } catch (IOException e){
-            logger.error("Error closing string writer: " + e.getMessage());
+        } catch (IOException e) {
+            logger.error("Error closing string writer: {}", e.getMessage());
         }
         return csvEventsString;
     }
 
     // private static String saveEventsToIcs(String eventsJson) throws IOException {
-    //     String icsEventsString = null;
-    //     try (StringWriter sw = new StringWriter();
-    //             PrintWriter writer = new PrintWriter(sw)) {
-    //         Gson gson = new Gson();
-    //         JsonObject jsonObject = gson.fromJson(eventsJson, JsonObject.class);
-    //         JsonArray events = jsonObject.getAsJsonArray("events");
+    // String icsEventsString = null;
+    // try (StringWriter sw = new StringWriter();
+    // PrintWriter writer = new PrintWriter(sw)) {
+    // Gson gson = new Gson();
+    // JsonObject jsonObject = gson.fromJson(eventsJson, JsonObject.class);
+    // JsonArray events = jsonObject.getAsJsonArray("events");
 
-    //         Calendar calendar = new Calendar();
-    //         calendar.add(new ProdId("-//Teamup//EN"));
-    //         Version v = new Version();
-    //         v.setValue(Version.VALUE_2_0);
-    //         calendar.add(v);
-    //         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+    // Calendar calendar = new Calendar();
+    // calendar.add(new ProdId("-//Teamup//EN"));
+    // Version v = new Version();
+    // v.setValue(Version.VALUE_2_0);
+    // calendar.add(v);
+    // DateTimeFormatter formatter =
+    // DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
 
-    //         for (int i = 0; i < events.size(); i++) {
-    //             JsonObject event = events.get(i).getAsJsonObject();
-    //             String uid = event.get("id").getAsString();
-    //             String summary = event.get("title").getAsString();
-    //             String start = event.get("start_dt").getAsString();
-    //             String end = event.get("end_dt").getAsString();
+    // for (int i = 0; i < events.size(); i++) {
+    // JsonObject event = events.get(i).getAsJsonObject();
+    // String uid = event.get("id").getAsString();
+    // String summary = event.get("title").getAsString();
+    // String start = event.get("start_dt").getAsString();
+    // String end = event.get("end_dt").getAsString();
 
-    //             OffsetDateTime startDateTime = OffsetDateTime.parse(start, formatter);
-    //             OffsetDateTime endDateTime = OffsetDateTime.parse(end, formatter);
+    // OffsetDateTime startDateTime = OffsetDateTime.parse(start, formatter);
+    // OffsetDateTime endDateTime = OffsetDateTime.parse(end, formatter);
 
-    //             VEvent vEvent = new VEvent();
-    //             vEvent.add(new DtStart<OffsetDateTime>(startDateTime));
-    //             vEvent.add(new DtEnd<OffsetDateTime>(endDateTime));
-    //             vEvent.add(new Summary(summary));
-    //             vEvent.add(new Uid(uid));
+    // VEvent vEvent = new VEvent();
+    // vEvent.add(new DtStart<OffsetDateTime>(startDateTime));
+    // vEvent.add(new DtEnd<OffsetDateTime>(endDateTime));
+    // vEvent.add(new Summary(summary));
+    // vEvent.add(new Uid(uid));
 
-    //             calendar.add(vEvent);
-    //         }
+    // calendar.add(vEvent);
+    // }
 
-    //         CalendarOutputter outputter = new CalendarOutputter();
-    //         outputter.output(calendar, writer);
-    //         icsEventsString = sw.toString();
-    //     } catch (IOException e) {
-    //         logger.error("Error closing string writer: " + e.getMessage());
-    //     }
-    //     return icsEventsString;
+    // CalendarOutputter outputter = new CalendarOutputter();
+    // outputter.output(calendar, writer);
+    // icsEventsString = sw.toString();
+    // } catch (IOException e) {
+    // logger.error("Error closing string writer: {}", e.getMessage());
+    // }
+    // return icsEventsString;
     // }
 
     // private static void saveEventsToFileJson(String eventsJson, String filePath)
@@ -315,9 +315,9 @@ public class TeamUpBackup {
     // Gson gson = new Gson();
     // JsonObject jsonObject = gson.fromJson(eventsJson, JsonObject.class);
     // gson.toJson(jsonObject, fileWriter);
-    // logger.info("Events saved to " + filePath);
+    // logger.info("Events saved to {}", filePath);
     // } catch (IOException e) {
-    // logger.error("Error saving events to file: " + e.getMessage());
+    // logger.error("Error saving events to file: {}", e.getMessage());
     // }
     // }
 }
